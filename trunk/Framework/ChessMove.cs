bd6625299e37fb98218674c5ac0cfbcdc3bab0f7
twiest <twiest@534bebff-002a-0410-b59a-f50d4d03ee1a>
2008-03-25 00:38:45 +0000

    * Got rid of all of the GTK code and references

--- trunk/Framework/ChessMove.cs
+++ trunk/Framework/ChessMove.cs
@@ -1,6 +1,6 @@
 /******************************************************************************
 * The MIT License
-* Copyright (c) 2006 Rusty Howell, Thomas Wiest
+* Copyright (c) 2008 Rusty Howell, Thomas Wiest
 *
 * Permission is hereby granted, free of charge, to any person obtaining  a copy
 * of this software and associated documentation files (the Software), to deal
--- trunk/Framework/Gui/LogWindow.cs
+++ trunk/Framework/Gui/LogWindow.cs
@@ -1,47 +1,10 @@
-#if USE_GTK
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Gtk;
-using Gdk;
 
 namespace UvsChess
 {
-    class LogWindow : Gtk.Window
+    class LogWindow
     {
-        public LogWindow(ChessColor playerColor)
-            : base("")
-        {
-            this.Title = playerColor.ToString();
-            Gtk.TextView view;
-            Gtk.TextBuffer buffer;
-            this.SetDefaultSize(400, 300);
-
-            view = new Gtk.TextView();
-            this.Add(view);
-            buffer = view.Buffer;
-
-            buffer.Text = playerColor.ToString() + " log";
-
-            this.ShowAll();
-
-            this.DeleteEvent += new DeleteEventHandler(LogWindow_DeleteEvent);//Close window event
-            this.Visible = false;
-
-
-        }
-
-        void LogWindow_DeleteEvent(object o, DeleteEventArgs args)
-        {
-            //Just hide the window. Don't destroy it.
-            //this.Visible = false;
-        }
-
-        public void Log(string message)
-        {
-        }
-           
     }
 }
-
-#endif
