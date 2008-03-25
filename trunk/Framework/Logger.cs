fab7f33067b3ef76c9d9bbf97ef722bbce9d8909
rustyhowell <rustyhowell@534bebff-002a-0410-b59a-f50d4d03ee1a>
2008-03-25 00:19:29 +0000

    * Committed Logger.cs and DllLoader.cs

---
+++ trunk/Framework/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UvsChess
+{
+    public class Logger
+    {
+        public static void Log(string msg)
+        {
+#if Console
+            Console.WriteLine(msg);
+#endif
+        }
+    }
+}
