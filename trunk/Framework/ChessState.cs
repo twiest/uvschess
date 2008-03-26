17c4435eb184c903fe6e4cb2ce7abdd69ea8b673
rustyhowell <rustyhowell@534bebff-002a-0410-b59a-f50d4d03ee1a>
2008-03-26 17:33:26 +0000

    * Added game play methods from GTK gui. * Stubbed out methods missing in GuiChessBoard class with NotImplementedException

--- trunk/Framework/ChessState.cs
+++ trunk/Framework/ChessState.cs
@@ -34,7 +34,7 @@ namespace UvsChess
     {
         #region Members
         public const string StartState = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-        //public const string StartState = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2";
+        //public const string StartState = "rnbqkbnr/1ppppppp/8/p7/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 2";
         private ChessBoard _currentBoard;
         private ChessBoard _previousBoard;
         private ChessMove _previousMove;
