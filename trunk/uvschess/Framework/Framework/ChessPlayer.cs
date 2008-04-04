63a93ba3d58e0b575c1bcb6687d086225c900e30
twiest <twiest@534bebff-002a-0410-b59a-f50d4d03ee1a>
2008-04-04 03:50:55 +0000

    * Removed all of the Logger.Logs from ChessPlayer

--- trunk/uvschess/Framework/Framework/ChessPlayer.cs
+++ trunk/uvschess/Framework/Framework/ChessPlayer.cs
@@ -78,19 +78,16 @@ namespace UvsChess.Framework
 
         public ChessMove GetNextMove(ChessBoard currentBoard)
         {
-            Logger.Log("In " + this.Color.ToString() + "'s GetNextMove.");
             _isMyTurn = true;
             _currentBoard = currentBoard.Clone();
 
             if (this.IsHuman)
             {
-                Logger.Log("In " + this.Color.ToString() + "'s GetNextMove and their human.");
                 _waitForMoveEvent.Reset();
                 _waitForMoveEvent.WaitOne();
             }
             else
             {
-                Logger.Log("In " + this.Color.ToString() + "'s GetNextMove and their an AI.");
                 _runAIThread = new Thread(GetNextAIMoveInThread);
 
                 // NO LOGGING ALLOWED between here
@@ -99,18 +96,14 @@ namespace UvsChess.Framework
                 _runAIThread.Start();
                 // AND HERE because it would count against the AI's time.
 
-                Logger.Log("In " + this.Color.ToString() + "'s GetNextMove and calling StartPollAI().");
                 this.PollAIOnce();
 
-                Logger.Log("In " + this.Color.ToString() + "'s GetNextMove waiting for the resetEvent to trigger.");
                 _waitForMoveEvent.Reset();
                 _waitForMoveEvent.WaitOne();
-                Logger.Log("In " + this.Color.ToString() + "'s GetNextMove and just got the resetEvent.");
 
                 _runAIThread = null;
             }
 
-            Logger.Log("In " + this.Color.ToString() + "'s GetNextMove and running GC Cleanup.");
             // Clean up the heap for the next player.
             // This doesn't cost the AI time 
             //(it's done after the AI's turn time has been calculated).
@@ -124,22 +117,18 @@ namespace UvsChess.Framework
 
         public void EndTurnEarly()
         {
-            Logger.Log("In " + this.Color.ToString() + "'s EndTurnEarly.");
             if (this.IsHuman)
             {
-                Logger.Log("In " + this.Color.ToString() + "'s EndTurnEarly and their human and firing the _waitForMoveEvent.");
                 _waitForMoveEvent.Set();
             }
             else
             {
-                Logger.Log("In " + this.Color.ToString() + "'s EndTurnEarly and their an AI and setting the _forceAIToEndTurnEarly flag to true.");
                 _forceAIToEndTurnEarly = true;
             }
         }
 
         private void PollAIOnce()
         {
-            Logger.Log("In " + this.Color.ToString() + "'s StartPollAI and _pollAITimer == null. Starting the Timer.");
             _pollAITimer = new Timer(this.PollAI, null, Interval, System.Threading.Timeout.Infinite);
         }
 
@@ -148,19 +137,6 @@ namespace UvsChess.Framework
             // NO LOGGING ALLOWED between here
             if ( (_forceAIToEndTurnEarly) || (!this.AI.IsRunning) || (DateTime.Now > _endTime) )
             {
-                //if (_forceAIToEndTurnEarly)
-                //{
-                //    Logger.Log("In " + this.Color.ToString() + "'s PollAI and telling the AI to end it's turn because it's been forced to.");
-                //}
-                //else if (!this.AI.IsGameRunning)
-                //{
-                //    Logger.Log("In " + this.Color.ToString() + "'s PollAI and telling the AI to end it's turn because it's done running, and it needs to cleanup.");
-                //}
-                //else
-                //{
-                //    Logger.Log("In " + this.Color.ToString() + "'s PollAI and telling the AI to end it's turn because it's time is over.");
-                //}
-
                 if (this.AI.IsRunning)
                 {
                     this.AI.IsRunning = false;
@@ -168,7 +144,6 @@ namespace UvsChess.Framework
                     int gracePeriod = Gui.Preferences.GracePeriod;
                     _pollAITimer = new Timer(this.GracePeriodTimer, null, gracePeriod, System.Threading.Timeout.Infinite);
 
-                    //Logger.Log("In " + this.Color.ToString() + "'s PollAI and Joining _runAIThread");
                     _runAIThread.Join();
                     
                 }
@@ -176,17 +151,13 @@ namespace UvsChess.Framework
                 TimeOfLastMove = DateTime.Now.Subtract(_startTime);
                 // AND HERE because it would count against the AI's time.
 
-                //Logger.Log("In " + this.Color.ToString() + "'s PollAI and firing the _waitForMoveEvent.");
                 _waitForMoveEvent.Set();
             }
             else
             {
-                Logger.Log("In " + this.Color.ToString() + "'s PollAI and setting it to poll one more time.");
                 // poll again
                 PollAIOnce();
             }
-
-            Logger.Log("In " + this.Color.ToString() + "'s PollAI [End].");
         }
 
         private void GracePeriodTimer(object state)
@@ -195,7 +166,6 @@ namespace UvsChess.Framework
             {
                 // The AI is still running, even after the grace period.
                 // They've now lost!
-                Logger.Log(this.ColorAndName + " has gone over the time limit and grace period. Having to abort his AI thread.");
                 _moveToReturn = new ChessMove(null, null);
                 _moveToReturn.Flag = ChessFlag.AIWentOverTime;
                 _runAIThread.Abort();
@@ -206,7 +176,6 @@ namespace UvsChess.Framework
         {
             if ( (_isMyTurn) && (this.IsHuman) )
             {
-                Logger.Log("Human Playing " + Color.ToString() + " moved:");
                 _moveToReturn = move;
                 _waitForMoveEvent.Set();
             }
