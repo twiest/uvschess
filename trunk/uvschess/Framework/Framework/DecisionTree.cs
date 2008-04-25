f7c4ef4187d2494bd8fbb4041162b79cb6d40129
twiest <twiest@534bebff-002a-0410-b59a-f50d4d03ee1a>
2008-04-25 07:43:21 +0000

    * Changed ChessBoard._board to be an automatic property Board * Created a new ChessBoard constructor that accepts ChessPiece[,] * Added DecisionTree which is our data structure to be able to debug what the AI is thinking during a turn

---
+++ trunk/uvschess/Framework/Framework/DecisionTree.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UvsChess.Framework
+{
+    public class DecisionTree
+    {
+        private ChessMove _move = null;
+        private ChessBoard _board = null;
+
+        public DecisionTree()
+        {
+            Level = 0;
+            Children = new List<DecisionTree>();
+        }
+
+        private DecisionTree(DecisionTree parent, ChessBoard board, ChessMove move, int level)
+        {
+            Children = new List<DecisionTree>();
+            Parent = parent;
+            Board = board;
+            Move = move;
+            Level = level;
+        }
+
+        public DecisionTree LastChild
+        {
+            get
+            {
+                return this.Children[this.Children.Count - 1];
+            }                
+        }
+
+        public DecisionTree FinalDecision
+        {
+            get;
+            private set;
+        }
+
+        private int Level
+        {
+            get;
+            set;
+        }
+
+        private List<DecisionTree> Children
+        {
+            get;
+            set;
+        }
+
+        public DecisionTree Parent
+        {
+            get;
+            private set;
+        }
+
+        public bool IsRootNode
+        {
+            get
+            {
+                return (this.Parent == null);
+            }
+        }
+
+        public ChessBoard Board
+        {
+            get
+            {
+                return _board;
+            }
+
+            set
+            {
+                _board = value.Clone();
+            }
+        }
+
+        public ChessMove Move
+        {
+            get
+            {
+                return _move;
+            }
+
+            set
+            {
+                _move = value.Clone();
+            }
+        }
+
+        public void AddChild(ChessBoard board, ChessMove move)
+        {
+            this.Children.Add(new DecisionTree(this, board, move, this.Level + 1));
+        }
+
+        public void AddFinalDecision(ChessBoard board, ChessMove move)
+        {
+            FinalDecision = new DecisionTree(this, board, move, 0);
+        }
+
+        //public void AddChild(ChessBoard board, ChessMove move)
+        //{
+        //    currentNode.Children.Add(new DecisionTree(currentNode, board, move, currentNode.Level + 1));
+        //}
+
+        //public void GoDownOneLevel()
+        //{
+        //    currentNode = currentNode.Children[currentNode.Children.Count - 1];
+        //}
+
+        //public void GoUpOneLevel()
+        //{
+        //    currentNode = currentNode.Parent;
+        //}
+    }
+}
