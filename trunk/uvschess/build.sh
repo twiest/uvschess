9b8aca994355d3e4c070e2885fad19ce4863b32d
rustyhowell <rustyhowell@534bebff-002a-0410-b59a-f50d4d03ee1a>
2008-05-14 05:53:15 +0000

    * Changed build.sh to use mdtool. * Removed build scripts from the individual projects.

--- trunk/uvschess/build.sh
+++ trunk/uvschess/build.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-#This builds both the UvsChess framework and the ExampleAI
+#This builds both the UvsChess solution, which includes the Framework, ExampleAI.
 
-
-./Framework/build.sh
-if [ $? -ne 0 ]
-then
-	exit 5
-fi	
-
-
-./ExampleAI/build.sh
-if [ $? -ne 0 ]
-then
-	exit 5
-fi	
+mdtool build -buildfile:UvsChess.sln
 
 
 
